476013959056229e10f66d48d0982a16cfdf74bd
Sriram <78687650+SriramRayala@users.noreply.github.com>
2024-03-19 13:35:27 +0530

    Update Vt-ip-url-analysis.py

--- Vt-ip-url-analysis.py
+++ Vt-ip-url-analysis.py
@@ -22,27 +22,36 @@ def url_report(arg):
     url = f"https://www.virustotal.com/api/v3/urls/{url_id}"
     headers = {"Accept": "application/json", "x-apikey": API_KEY}
     response = requests.get(url, headers=headers)
-    decoded_response = json.loads(response.text)
+    decoded_response = response.json()  # Convert response to JSON
     timestamp = time.strftime('%c', time.localtime(time.time()))
-    filtered_response = decoded_response["data"]["attributes"]
-    keys_to_remove = [...]
-    for key in keys_to_remove:
-        filtered_response.pop(key, None)
-    df = pd.DataFrame.from_dict(filtered_response, orient="index")
-    df.columns = [target_url]
-    epoch_time = decoded_response["data"]["attributes"]["last_analysis_date"]
-    time_formatted = time.strftime('%c', time.localtime(epoch_time))
-    url_id_unencrypted = f"http://{target_url}/"
-    sha_signature = hashlib.sha256(url_id_unencrypted.encode()).hexdigest()
-    vt_url_report_link = f"https://www.virustotal.com/gui/url/{sha_signature}"
-    community_score = decoded_response["data"]["attributes"]["last_analysis_stats"]["malicious"]
-    total_vt_reviewers = sum(decoded_response["data"]["attributes"]["last_analysis_stats"].values())
-    community_score_info = f"{community_score}/{total_vt_reviewers} : security vendors flagged this as malicious"
-    df.loc['virustotal report', :] = vt_url_report_link
-    df.loc['community score', :] = community_score_info
-    df.loc['last_analysis_date', :] = time_formatted
-    df.sort_index(inplace=True)
-    return df
+    
+    # Check if 'data' key exists in the response
+    if 'data' in decoded_response:
+        filtered_response = decoded_response["data"]["attributes"]
+        
+        # Rest of the code remains the same
+        keys_to_remove = [...]
+        for key in keys_to_remove:
+            filtered_response.pop(key, None)
+        df = pd.DataFrame.from_dict(filtered_response, orient="index")
+        df.columns = [target_url]
+        epoch_time = decoded_response["data"]["attributes"]["last_analysis_date"]
+        time_formatted = time.strftime('%c', time.localtime(epoch_time))
+        url_id_unencrypted = f"http://{target_url}/"
+        sha_signature = hashlib.sha256(url_id_unencrypted.encode()).hexdigest()
+        vt_url_report_link = f"https://www.virustotal.com/gui/url/{sha_signature}"
+        community_score = decoded_response["data"]["attributes"]["last_analysis_stats"]["malicious"]
+        total_vt_reviewers = sum(decoded_response["data"]["attributes"]["last_analysis_stats"].values())
+        community_score_info = f"{community_score}/{total_vt_reviewers} : security vendors flagged this as malicious"
+        df.loc['virustotal report', :] = vt_url_report_link
+        df.loc['community score', :] = community_score_info
+        df.loc['last_analysis_date', :] = time_formatted
+        df.sort_index(inplace=True)
+        return df
+    else:
+        print("Error: 'data' key not found in response")
+        return None
+
 
 def url_report_lst(arg):
     with open(arg) as f:
